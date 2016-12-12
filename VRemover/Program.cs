804e6d8dff6393df1b4ab0f2a9c8ebfd427a6f8c
michelle <michelle.lunoza118@gmail.com>
2016-12-12 20:45:02 +0800

    update

---
+++ VRemover/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Chaelgutierrez
+ * Date: 12/12/2016
+ * Time: 8:12 PM
+ * 
+ */
+using System;
+using System.Diagnostics;
+namespace VRemover
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Process cmd = new Process();
+			cmd.StartInfo.FileName = "cmd.exe";
+			cmd.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+			cmd.StartInfo.RedirectStandardInput = true;
+			cmd.StartInfo.RedirectStandardOutput = true;
+			cmd.StartInfo.CreateNoWindow = true;
+			cmd.StartInfo.UseShellExecute = false;
+			cmd.Start();
+			
+			cmd.StandardInput.WriteLine("start notepad");
+			cmd.StandardInput.WriteLine("start calc");
+			cmd.StandardInput.WriteLine("start chrome");
+			
+			
+			//cmd.StandardInput.Flush();
+			//cmd.StandardInput.Close();
+			//cmd.WaitForExit();
+			//Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+			//Console.Write("Press any key to continue . . . ");
+			//Console.ReadKey(true);
+		}
+	}
+}
